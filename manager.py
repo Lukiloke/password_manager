b942abc9f6064d7bff55575a9855242ec42cb51c
Lukiloke <189272386+Lukiloke@users.noreply.github.com>
2025-10-20 20:27:54 +0200

    ajout V1

---
+++ manager.py
@@ -0,0 +1,125 @@
+from cryptography.fernet import Fernet 
+import hashlib
+import base64
+import os
+
+def clear_history():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def converting_password(password: str) -> bytes:
+    key1 = password
+    hashed_key = hashlib.sha256(key1.encode('utf-8'))
+    hashed_key_digest = hashed_key.digest()
+    base64_key = base64.b64encode(hashed_key_digest)
+    return base64_key
+
+
+def encrypt_file():
+    original_data = open(filename, "rb").read()
+    encrypted_data = f.encrypt(original_data)
+    deletion = open(filename,'r+')
+    deletion.truncate(0)
+    with open(filename, 'wb') as file:
+        file.write(encrypted_data)
+    file.close()
+
+def decrypt_file():
+    encrypted_file = open(filename, "rb").read()
+    decrypted_data = f.decrypt(encrypted_file)
+    with open(filename, 'wb') as file:
+        file.write(decrypted_data)
+        file.close()
+
+def decrypt_variable_mode():
+    encrypt_file = open(filename, 'rb').read()
+    decrypted_data_vmode = f.decrypt(encrypt_file)
+    out_vmode = decrypted_data_vmode.decode('utf-8')
+    return out_vmode
+
+def read_file_content():
+    content = open(filename, "rb").read()
+    content = content.decode('utf-8')
+    return content
+
+def write_file():
+    data_w = "\n"+ input('Data to write:  ') 
+    with open(filename,'ab') as file:
+        file.write(data_w.encode('utf-8'))
+        file.close()
+
+def clear_file():
+    data = open(filename, 'rb+')
+    data.truncate(0)
+
+def add_password():
+    domain = input("App/Site:   ")
+    login_details = input("Login Id:    ")
+    password = input("Password:     ")
+    additionals = input("Additionals? Y/n :     ")
+    additional_data = ""
+    if additionals == "Y":
+        additional_data == input("Enter Additional infos you want to add with this site:   ")
+    else:
+        pass
+    to_write = "\n"+ domain + "   " + login_details + "   " + password + "   " + additional_data
+    to_write = to_write.encode('utf-8')
+    with open(filename, 'ab') as file:
+        file.write(f.encrypt(to_write))
+        file.close()
+    
+
+clear_history()
+key = (converting_password(input("Enter password (note that if you try decrypting using a wrong password the program may crash) :    ")))
+clear_history()
+f = Fernet(key)
+path= str(os.path.join(os.path.expanduser('~'), 'Documents'))+ r'\Manager.txt'
+with open(path, 'a+') as file:
+    file.close()
+    pass
+ 
+
+filename = path
+option_loop = 0
+while option_loop == 0:
+    options = input( "Here are the available command :\n$ encrypt\n$ decrypt\n$ decrypt_vmode\n$ add_password\n$ file_content\n$ write_file\n$ clear_file\n$ exit\n\n      $ ")
+    if options == "encrypt":
+        clear_history()
+        encrypt_file()
+        print(">     File successfully encrypted !")
+
+    elif options == "decrypt_vmode":
+        clear_history()
+        print(">     "+decrypt_variable_mode())
+
+    elif options == "decrypt":
+        clear_history()
+        decrypt_file()
+        print(">     File successfully decrypted !")
+
+    elif options == "file_content":
+        clear_history()
+        print('>     '+read_file_content())
+        
+    elif options == "write_file":
+        clear_history()
+        write_file()
+
+    elif options == "clear_file":
+        clear_history()
+        clear_file()
+        print(">     File size set to 0 ")
+
+    elif options == "add_password":
+        clear_history()
+        add_password()
+
+    elif options == "exit":
+        break
+        
+
+    else:
+        clear_history()
+        print(">     "+"Command doesn't exist, try again.")
+
+    input()
+    clear_history()
